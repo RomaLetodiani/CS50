86827c30c0898529ee19e168b124e475ce2f8937
RomaLetodiani <roman.letodiani03@gmail.com>
2024-05-03 12:11:48 +0400

    Update scrabble.c

--- CS50X/Week 2/Assignments/Scrabble/scrabble.c
+++ CS50X/Week 2/Assignments/Scrabble/scrabble.c
@@ -1,4 +1,3 @@
-#include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
@@ -6,13 +5,16 @@
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int compute_score(string word);
+int compute_score(char *word);
 
 int main(void)
 {
+    char word1[20], word2[20];
     // Prompt the user for two words
-    string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
+    printf("Player 1: ");
+    scanf("%s", word1);
+    printf("Player 2: ");
+    scanf("%s", word2);
 
     // Compute the score of each word
     int score1 = compute_score(word1);
@@ -33,7 +35,7 @@ int main(void)
     }
 }
 
-int compute_score(string word)
+int compute_score(char *word)
 {
     // Keep track of score
     int score = 0;
