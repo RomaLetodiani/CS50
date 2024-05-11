af5922f2fd158b5e8a6b4d86de16fdf655b7c316
RomaLetodiani <roman.letodiani03@gmail.com>
2024-05-11 12:00:33 +0400

    Create nutrition.py

---
+++ CS50P/Week 2/Assignments/nutrition/nutrition.py
@@ -0,0 +1,33 @@
+def main():
+    user_input = input("Item: ")
+    print(helper(user_input.lower()))
+
+
+def helper(s: str):
+    fruits = {
+        'apple': 130,
+        'avocado': 50,
+        'banana': 110,
+        'cantaloupe': 50,
+        'grapefruit': 60,
+        'grapes': 90,
+        'honeydew': 50,
+        'kiwifruit': 90,
+        'lemon': 20,
+        'lime': 20,
+        'nectarine': 60,
+        'orange': 80,
+        'peach': 60,
+        'pear': 100,
+        'pineapple': 50,
+        'plums': 70,
+        'strawberries': 50,
+        'sweet cherries': 100,
+        'tangerine': 50,
+        'watermelon': 80,
+    }
+    return fruits.get(s, '')
+
+
+if __name__ == "__main__":
+    main()
